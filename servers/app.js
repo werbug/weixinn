998c15c650dc9ca120998cfc3a2ed1d9f88a5021
svon <svon@svon.org>
2016-10-31 21:53:32 +0800

    node weixin servers

---
+++ servers/app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = new express();
+
+var path = require("path");
+
+var saticpath = path.join(__dirname , "../static");
+app.use(express.static(saticpath));
+
+app.listen(3000,function(){
+	console.log("open http://127.0.0.1:3000");
+})
