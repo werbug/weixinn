d5c4101d79eba94b3f52c30e2d4ae49c480e15d4
svon <svon@svon.org>
2016-11-01 21:30:01 +0800

    token

---
+++ servers/config.js
@@ -0,0 +1,15 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wx116353cb702a6584',
+		//AppSecret(应用密钥)
+		appsecret: 'd843b8cbfecdd2fe816e3d353a411ec6',
+		//Token(令牌)
+		token: 'wx_svon_me'
+	};
+};
