236210139d58542d84f9225fd1cb7fc00263fcf6
svon <svon@svon.org>
2016-11-05 15:09:43 +0800

    location

--- static/test.js
+++ static/test.js
@@ -20,7 +20,8 @@ $(function(){
             'onMenuShareQQ',
             'onMenuShareWeibo',
             //获取查看用户网络状态权限
-            'onGetNetworkType'
+            'onGetNetworkType',
+            'onGetLocation'
 		].join(",")
 	},function(resp){
 		wx.config({
@@ -49,6 +50,26 @@ $(function(){
 		    	// 返回网络类型2g，3g，4g，wifi
 		        var networkType = res.networkType;
 		        alert(networkType);
+		    }
+		});
+	});
+	
+	$("#getLocation").on('click',function(){
+		wx.getLocation({
+		    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+		    success: function (res) {
+		        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+		        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+		        var speed = res.speed; // 速度，以米/每秒计
+		        var accuracy = res.accuracy; // 位置精度
+		        
+		        var text = [
+		        	"纬度 ：" + latitude,
+		        	"经度：" + longitude,
+		        	"位置精度 ：" + accuracy
+		        ]
+		        
+		        alert(text.join("\n"));
 		    }
 		});
 	});
