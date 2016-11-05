f0ed92ec31a8aff42a9836777892c4d7b849b2fe
svon <svon@svon.org>
2016-11-05 12:50:26 +0800

    fix

--- servers/app.js
+++ servers/app.js
@@ -10,6 +10,6 @@ require("./jssdk.js")(app);
 var saticpath = path.join(__dirname , "../static");
 app.use(express.static(saticpath));
 
-app.listen(8000,function(){
+app.listen(9876,function(){
 	console.log("open http://127.0.0.1:80");
 });
