1b4dd654548b572bf8771212c93295037ce842bc
svon <svon@svon.org>
2016-11-05 12:38:42 +0800

    jssdk

--- servers/app.js
+++ servers/app.js
@@ -1,6 +1,13 @@
 var express = require("express");
+//post 请求处理模块
+var bodyparser = require("body-parser");
 
 var app = new express();
+//处理 post 请求，将 post 请求的数据封装为 json 
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+
 
 var path = require("path");
 
