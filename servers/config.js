5d91d5122495226744d7c1d239470da68e1eb9c9
svon <svon@svon.org>
2016-11-05 15:16:53 +0800

    微信测试账号

--- servers/config.js
+++ servers/config.js
@@ -4,11 +4,21 @@
  * 注：测试时请替换您真实的数据
  */
 module.exports = function(){
+	//个人订阅号
+//	return {
+//		//AppID(应用ID)
+//		appid: 'wx116353cb702a6584',
+//		//AppSecret(应用密钥)
+//		appsecret: 'd843b8cbfecdd2fe816e3d353a411ec6',
+//		//Token(令牌)
+//		token: 'wx_svon_me'
+//	};
+	//微信测试号
 	return {
 		//AppID(应用ID)
-		appid: 'wx116353cb702a6584',
+		appid: 'wx916ac337bf3c3c22',
 		//AppSecret(应用密钥)
-		appsecret: 'd843b8cbfecdd2fe816e3d353a411ec6',
+		appsecret: '382dd0502bea061e8329ff7c1e83e7dc',
 		//Token(令牌)
 		token: 'wx_svon_me'
 	};
